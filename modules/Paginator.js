a6e82432aa0b3d40304e7c325a6499c0fed75eb8
Dean Sofer <ProLoser@users.noreply.github.com>
2015-06-18 11:28:24 -0700

    Added kickass Paginator example
    Cleaned up logic that keeps querying responsibilities in the ObjectClass

--- modules/Paginator.js
+++ modules/Paginator.js
@@ -1,39 +1,67 @@
-angular.module('App').factory('Paginator', function($http){
+/**
+ * Paginator - Simple paginator utility example that abstracts logic in a controllable pattern
+ * 
+ * @param paginate {function} Query function that takes paginationOptions
+ * 
+ * @example
+ *   resolve: {
+ *     // Prepares the paginator
+ *     paginator: function(Paginator, Project) {
+ *       // Calls `Project.list(paginationOptions)`
+ *       return new Paginator(Project.list, { limit: 50 });
+ *     },
+ *     // Queries the initial load
+ *     projects: function(paginator) {
+ *       return paginator.more();
+ *     }
+ *   }
+ * 
+ * @example
+ *   resolve: {
+ *     taskPaginator: function(Paginator, Task, $stateParams) {
+ *       return new Paginator(function(paginationOptions){
+ *         return Task.list($stateParams.projectId, paginationOptions);
+ *       });
+ *     },
+ *     tasks: function(taskPaginator) {
+ *       return taskPaginator.more();
+ *     }
+ *   }
+ */
+angular.module('App').factory('Paginator', function($q){
 
-  var Paginator = function(url, options){
-    this.url = url;
-    this.options = {
-      limit: 50,
-      offset: 0
-    };
-    //add everything to the this.params object
-    for (var name in options){
-      this.options[name] = options[name];
+  class Paginator {
+    
+    constructor(paginate, options) {
+      this.paginate = paginate;
+      this.items = [];
+      this.hasMore = true;
+      this.loading = false;
+      this.options = Object.assign({
+        limit: 20,
+        offset: 0
+      }, options);
     }
-    this.items = [];
-    this.hasMore = true;
-    this.loading = false;
-    //make first call for items at initialization
-    this.next();
-  };
-
-  Paginator.prototype.next = function(){
-    var self = this;
-
-    this.loading = true;
-
-    if (this.hasMore) {
-      return $http.get(this.url, { params: this.options }).then(function(response) {
-        //If the results are less than the required limit then the results are finished
-        if(response.data.results.length < self.options.limit){
-          self.hasMore = false
-        }
-        self.items = self.items.concat(response.data.results);
-        self.options.offset = self.items.length;
-        self.loading = false;
-        return self.items;
-      });
+    
+    more() {
+      this.loading = true;
+  
+      if (this.hasMore) {
+        return this.paginate(this.options).then((response) => {
+          //If the results are less than the required limit then the results are finished
+          if(response.data.results.length < self.options.limit){
+            this.hasMore = false
+          }
+          this.items = this.items.concat(response.data.results);
+          this.options.offset = this.items.length;
+          this.loading = false;
+          return this.items;
+        });
+      } else {
+        return $q.when(this.items);
+      }
     }
+    
   }
 
   return Paginator;

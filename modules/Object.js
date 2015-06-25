6782ae31e7db3016e391502eee3807cecaf5253d
Dean Sofer <ProLoser@users.noreply.github.com>
2015-06-24 23:39:51 -0700

    Update Object.js

--- modules/Object.js
+++ modules/Object.js
@@ -2,9 +2,13 @@ var module = angular.module('App');
 
 module.factory('BaseObject', () => {
   class BaseObject {
-    /*
-    Creates a new instance from JSON.
-    */
+    /**
+     * BaseObject.new() - a wrapper for `new BoseObject()` for use in callbacks
+     * @param  {hash} data Initialization data
+     * @return {object}    The newly created object
+     * @example
+     * 	$http.get('/api/items').then( response => response.data.map(Item.new) )
+     */
     static new(data) {
       return new this(data);
     }
@@ -14,21 +18,23 @@ module.factory('BaseObject', () => {
       Object.assign(this, data);
     }
 
-    /*
-    Switch to high-memory mode
-
-    Usually means it's being rendered on screen and should get extra details and keep them updated
-    */
+    /**
+     * Switch to high-memory mode
+     *
+     * Usually means it's being rendered on screen and should get extra details and keep them updated
+     */
     open() {}
 
-    /*
-    Switch to low-memory mode
-
-    Unsubscribe unnecessary overhead
-    */
+    /**
+     * Switch to low-memory mode
+     *
+     * Unsubscribe unnecessary overhead
+     */
     close() {}
 
-    // Convenience wrapper
+    /**
+     * Convenience wrapper
+     */
     save() {
       if (this.id) {
         return this.update();
@@ -43,22 +49,24 @@ module.factory('BaseObject', () => {
 
     delete() {}
 
-    /*
-    Make a copy of object for use in forms
-
-    When you edit a record in a form, you want the original to be preserved while the user makes changes
-    This allows you to edit a record exactly as if you were creating one without having to worry about
-    rolling back changes to the object.
-    */
+    /**
+     * Make a copy of object for use in forms
+     * 
+     * When you edit a record in a form, you want the original to be preserved while the user makes changes
+     * This allows you to edit a record exactly as if you were creating one without having to worry about
+     * rolling back changes to the object.
+     * 
+     * @note This makes a shallow copy only
+     */
     clone() {
       var constructor = Object.getPrototypeOf(this).constructor;
       // TODO: add support for queries by passing parent(?)
       return new constructor(this);
     }
 
-    /*
-    Cleans up listeners (should run when discarding object)
-    */
+    /**
+     * Cleans up listeners (should run when discarding object)
+     */
     destroy() {}
   }
 

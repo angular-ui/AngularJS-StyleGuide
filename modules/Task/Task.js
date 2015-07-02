6eb53916a3c3c95db5b495c97a775e87aa994e52
Dean Sofer <ProLoser@users.noreply.github.com>
2015-07-01 22:35:34 -0700

    Update Task.js

--- modules/Task/Task.js
+++ modules/Task/Task.js
@@ -97,16 +97,38 @@ module.factory( 'Task', (BaseObject, $http) => {
     }
 
 
+    /**
+     * task.create() - Creates a new task
+     * 
+     * @note Demonstrates how to have the create wait until uploading is complete
+     *   Normally you might have to wait until the attachment is done before enabling
+     *   submission of the form, but this way allows the user to submit before uploading
+     *   is complete and makes it easy to show loading spinners.
+     * 
+     *   You could check either the `task.saving`, `task.creating`, or `task.uploading`
+     *   properties for truthy value when the user tries to navigate away from the page.
+     * 
+     */
     create() {
-      return $http.post('/api/tasks', this)
-        .then( (response) => {
-          this.id = response.data.id;
-          return response.data;
-        });
+      // wraps `this.uploading` in a promise that resolves immediately if it is `null` or waits
+      return this.creating = $q.when(this.uploading)
+        .then( () => $http.post('/api/tasks', this) ) // uploading callback
+        .then( response => return Object.assign(this, response.data) ) // creating callback
+        .finally( () => this.creating = null ); // state cleanup (doesn't affect chaining)
     }
 
     update() {
       return $http.put(`/api/tasks/${this.id}`, this);
+    }
+    
+    /**
+     * task.upload() - Allow you to upload attachments to issues
+     * 
+     * @note Added to demonstrate clean ways to have 1 method wait for another method to finish
+     */
+    upload(attachment) {
+      return this.uploading = $http.post(`/api/tasks/${this.id}`, attachment)
+        .finally( () => this.uploading = null ); // state cleanup (doesn't affect chaining)
     }
   }
 

3818ea6ce0fbf7c2c7e3a8acaa56b6e935d69bf5
Dean Sofer <ProLoser@users.noreply.github.com>
2015-07-02 17:15:57 -0700

    Update Paginator.js

--- modules/Paginator.js
+++ modules/Paginator.js
@@ -28,10 +28,20 @@
  *     }
  *   }
  */
-angular.module('App').factory('Paginator', function($q){
+angular.module('App').factory('Paginator', function($http, $q){
+
   class Paginator {
-    constructor(paginate, options) {
+    /**
+     * @param  {string|function} paginate URL or callback function that returns
+     *                                    a promise
+     * @param  {object} options Default paginate query options
+     * @param  {object} relatedHelpers Map of callback functions that take array
+     *                                 of items and returns an indexed hash
+     */
+    constructor(paginate, options = {}, relatedHelpers = {}) {
       this.paginate = paginate;
+      this.relatedHelpers = relatedHelpers;
+      this.related = {};
       this.options = _.extend({
         limit: 50,
         offset: 0
@@ -45,16 +55,41 @@ angular.module('App').factory('Paginator', function($q){
       if (!this.hasMore) return $q.when();
       if (this.loading) return this.loading;
 
-      return this.loading = this.paginate(this.options)
-        .then( response => {
-          if (response.data.results.length < this.options.limit)
-            this.hasMore = false;
-  
-          this.items.push.apply(this.items, response.data.results);
-          this.options.offset = this.items.length;
-          this.loading = null;
-          return this.items;
-        });
+      return this.loading = this.paginate(this.options).then( items => {
+        if (items.length < this.options.limit)
+          this.hasMore = false;
+
+        this.items.push.apply(this.items, items);
+        this.options.offset = this.items.length;
+        this.loading = null;
+        return this.getRelated(items);
+      });
+    }
+
+    /**
+     * getRelated(newItems)
+     *
+     * Iterates over related data retrieval helpers
+     * When each helper resolves with a hash of relatedItems, they are merged onto
+     * the paginator's existing cache of related items.
+     *
+     * @example
+     *   paginator = new Paginator(Project.list(), {}, { owners: Project.relatedOwners });
+     *   paginator.next();
+     *
+     *   <li ng-repeat="project in paginator.projects">
+     *     {{paginator.related.owners[project.owner_id].name}}
+     *   </li>
+     *
+     * @param  {array} [items] an array of objects to pass to the related helper
+     * @return {Promise}       resolved when all helpers are done
+     */
+    getRelated(items = this.items) {
+      return $q.all(_.mapValues(this.relatedHelpers, (helper, name) =>
+        helper(items).then( relatedItems =>
+          this.related[name] = _.extend(this.related[name] || {}, relatedItems)
+        )
+      ));
     }
 
     /**
@@ -76,5 +111,6 @@ angular.module('App').factory('Paginator', function($q){
     }
 
   }
+  
   return Paginator;
 });

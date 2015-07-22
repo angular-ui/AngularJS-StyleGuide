e614905b67f35ac04d6535ddef42bbf820dfbeea
Dean Sofer <ProLoser@users.noreply.github.com>
2015-07-22 14:59:38 -0700

    Update User.js

--- modules/User/User.js
+++ modules/User/User.js
@@ -70,33 +70,6 @@ module.controller( 'UserForm', ($scope, user, wizard) => {
   $scope.wizard = wizard;
 });
 
-module.factory( 'User', (BaseObject, $http) => {
-  class User extends BaseObject {
-    static list() {
-      return $http.get('/api/users')
-        .then( (response) => response.data.map(User.new));
-    }
-
-
-    // checks validity of the property (probably should be in BaseObject)
-    validate(property) {
-      if (!User.rules[property])
-        return true;
-
-      var pattern = new RegExp(User.rules[property]);
-      return pattern.test(this[property]);
-    }
-  }
-
-  User.rules = {
-    name: '[a-zA-Z0-1]{4,}', // 4 or more alphanum chars
-    email: '.+this.gmail.com'
-  };
-
-  return User;
-});
-
-
 module.factory( 'RegistrationWizard', () => {
   class RegistrationWizard {
     constructor(user) {

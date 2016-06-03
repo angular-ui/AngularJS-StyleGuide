e4ede6dc99126a1dc711e89b716780d1fcf96f9d
Dean Sofer <ProLoser@users.noreply.github.com>
2016-06-03 14:18:18 -0700

    Update TaskObject.js

--- modules/Task/TaskObject.js
+++ modules/Task/TaskObject.js
@@ -3,7 +3,7 @@ module.factory( 'Task', (BaseObject, $http) => {
   class Task extends BaseObject {
     static list(projectId) {
       return $http.get('/api/tasks', { params: { project_id: projectId } })
-        .then( (response) => response.data.map( task => new Task(task) ) );
+        .then( response => response.data.map( task => new Task(task) ) );
     }
 
 

f3655b0773b157186ae485c7c0cb3c17ebdb003f
Dean Sofer <ProLoser@users.noreply.github.com>
2018-02-22 13:12:40 -0800

    Update UserObject.js

--- modules/User/UserObject.js
+++ modules/User/UserObject.js
@@ -1,19 +1,44 @@
 var module = angular.module('App.User');
 module.factory( 'User', (BaseObject, $http) => {
   class User extends BaseObject {
+    /**
+     * Get list of users
+     * 
+     * @returns {Promise<User[]>}
+     */
     static list() {
       return $http.get('/api/users')
-        .then( (response) => response.data.map(User.new));
+        .then( (response) => response.data.map( user => new User(user) ));
     }
 
 
-    // checks validity of the property (probably should be in BaseObject)
+    /**
+     * Validity function for use in the view / form
+     *
+     * @note Client-side validation is a convenience, server-side validation is imperative 
+     */
     validate(property) {
       if (!User.rules[property])
         return true;
 
       var pattern = new RegExp(User.rules[property]);
       return pattern.test(this[property]);
+    }
+    
+    create() {
+      return this.cache('creating', () => 
+        $http.post('/api/users', this)
+          // Attach additional data from server to this object
+          .then( data => Object.assign(this, data) )
+      );
+    }
+    
+    update() {
+      return this.cache('updating', () => 
+        $http.put('/api/users', this)
+          // Attach additional data from server to this object
+          .then( data => Object.assign(this, data) )
+      );
     }
   }
 

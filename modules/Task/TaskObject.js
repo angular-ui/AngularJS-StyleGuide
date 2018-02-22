6bbc87b9963fb05cbc0ffaa3f5ef096b2d3fd71c
Dean Sofer <ProLoser@users.noreply.github.com>
2018-02-22 13:15:57 -0800

    Update TaskObject.js

--- modules/Task/TaskObject.js
+++ modules/Task/TaskObject.js
@@ -1,6 +1,11 @@
 module = angular.module('App.Task');
 module.factory( 'Task', (BaseObject, $http) => {
   class Task extends BaseObject {
+    /**
+     * Retrieve tasks from a project
+     * @param {mixed} projectId
+     * @returns {Promise<task[]>}
+     */
     static list(projectId) {
       return $http.get('/api/tasks', { params: { project_id: projectId } })
         .then( response => response.data.map( task => new Task(task) ) );
@@ -8,7 +13,7 @@ module.factory( 'Task', (BaseObject, $http) => {
 
 
     /**
-     * task.create() - Creates a new task
+     * Creates a new task
      * 
      * @note Demonstrates how to have the create wait until uploading is complete
      *   Normally you might have to wait until the attachment is done before enabling
@@ -18,6 +23,7 @@ module.factory( 'Task', (BaseObject, $http) => {
      *   You could check either the `task.saving`, `task.creating`, or `task.uploading`
      *   properties for truthy value when the user tries to navigate away from the page.
      * 
+     * @returns {Promise}
      */
     create() {
       // wraps `this.uploading` in a promise that resolves immediately if it is `null` or waits for the promise
@@ -28,19 +34,26 @@ module.factory( 'Task', (BaseObject, $http) => {
       );
     }
 
+    /**
+     * Update the task
+     *
+     * @returns {Promise}
+     */
     update() {
       // wraps `this.uploading` in a promise that resolves immediately if it is `null` or waits for the promise
       return this.cache('updating', () =>
         $q.when(this.uploading)
-          .then( () => $http.post(`/api/tasks/${this.id}`, this) ) // uploading callback
+          .then( () => $http.put(`/api/tasks/${this.id}`, this) ) // uploading callback
           .then( response => Object.assign(this, response.data) ) // creating callback
       );
     }
     
     /**
-     * task.upload() - Allow you to upload attachments to issues
+     * Allow you to upload attachments to tasks
      * 
      * @note Added to demonstrate clean ways to have 1 method wait for another method to finish
+     * @param {mixed} attachment
+     * @returns {Promise}
      */
     upload(attachment) {
       return this.cache('uploading', () => 
